88fcc1ab36d01acaadbd157976b862270f9f9a07
Aryan Saraswat <aryan.saraswat@hotmail.co.uk>
2021-12-29 11:44:09 +0530

    added beautifulsoup lecture database scraper

---
+++ DegreesDataEn.py
@@ -0,0 +1,57 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+page_url = 'https://www.uni-due.de/vdb/en_EN/studiengang/liste'
+
+# opens the connection and downloads html page from url
+uClient = uReq(page_url)
+
+# parses html into a soup data structure to traverse html
+# as if it were a json data type.
+page_soup = soup(uClient.read(), "html.parser")
+uClient.close()
+
+studyProgramContainers = page_soup.findAll("div", {"class": "highlight-blue"})
+# name the output file to write to local disk
+out_filename = "Courses_data.csv"
+# header of csv file to be written
+headers = "Study Program,Courses,Description\n"
+
+# opens file, and writes headers
+f = open(out_filename, "w")
+f.write(headers)
+
+studyProgramContainer = studyProgramContainers[0]
+print(studyProgramContainer)
+for studyProgram in studyProgramContainer.select("a"):
+    if(studyProgram.text.find("B.Sc.") != -1):
+        page_url = 'https://www.uni-due.de' + studyProgram['href']
+
+        # opens the connection and downloads html page from url
+        uClient = uReq(page_url)
+
+        # parses html into a soup data structure to traverse html
+        # as if it were a json data type.
+        page_soup = soup(uClient.read(), "html.parser")
+        uClient.close()
+
+        # finds each product from the store page
+        containers = page_soup.findAll("fieldset", {"class": "highlight-yellow"})
+        for container in containers:
+            for courses in container.select("a"):
+                course = courses.text
+                if(courses.text.find("Dr.") != -1 or courses.text.find("M.Sc.") != -1 or courses.text.find("Prof.") != -1 ):
+                    continue
+                else:
+                    page_url = 'https://www.uni-due.de' + courses['href']
+                    uClient = uReq(page_url)
+                    page_soup = soup(uClient.read(), "html.parser")
+                    uClient.close()
+                    coursesContainers= page_soup.findAll("div", {"class": "highlight-blue"})
+                    coursesContainer = coursesContainers[1].findAll("td")
+                    description = coursesContainer[1].text
+                    if(description != ""):
+                        f.write(studyProgram.text.replace(",", " ") +"," +course.replace(",", " ") +","+ description.replace(",", " ").replace('\n', ' ')  +"\n")
+
+
+f.close()  # Close the file
