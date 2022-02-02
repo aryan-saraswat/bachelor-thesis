7a6db7d89f4f06dd3e87fd31f51e607d846b45c2
Aryan Saraswat <aryan.saraswat@hotmail.co.uk>
2022-02-02 13:24:42 +0530

    pipeline now merges vdb and lsf data, assigns einzeltermine to lectures

--- scrapers/merge_lsf_and_vdb.py
+++ scrapers/merge_lsf_and_vdb.py
@@ -1,6 +1,7 @@
 import io
 import json
 from difflib import SequenceMatcher
+from pprint import pprint
 
 lsf_data_directory = '.\\lsf_scraper\\lsf_scraper\\Data\\post_processed_lectures.json'
 vdb_data_directory = '.\\vdb_scraper\\vdb_scraper\\Data\\post_processed_descriptions.json'
@@ -12,7 +13,7 @@ def similar(name1, nameList):
         if SequenceMatcher(None, name1, name).ratio() > ratio:
             ratio = SequenceMatcher(None, name1, name).ratio()
             result = name
-    if ratio > 0.75:
+    if ratio > 0.60:
         return (result, ratio)
     else:
         return (None, None)
@@ -29,22 +30,31 @@ with io.open(vdb_data_directory, encoding='UTF8') as vdb_data, io.open(lsf_data_
 
     lecture_name_list = list(vdb_json.keys())
     print(lecture_name_list)
+    distant_matches = []
 
     for lsf_id, lsf_value in lsf_json.items():
         subject = lsf_value['name']
-        if subject in vdb_json.keys():
+        if 'zu' in subject:
+            subject = ' '.join(subject.split(' ')[2:]).replace('"', '')
+        if subject in vdb_json.keys(): # checking if the subject from the lsf_data is in the keys of the vdb dictionary
             matches = matches + 1
-            print('exact match:     {}   ---->   {}'.format(subject, lsf_value['name']))
+            # print('exact match:\t{}\t---->\t{}'.format(subject, lsf_value['name']))
         else:
             result = similar(subject, lecture_name_list)
             closest_match, ratio = result[0], result[1]
             if not closest_match:
                 similarity_too_low = similarity_too_low + 1
-                print('no match for:     {}'.format(subject))
+                print('no match for:\t{}'.format(subject))
             else:
                 somewhat_same = somewhat_same + 1
-                print('closest match for:     {}   ---->   {}  with ratio   {}'.format(subject, lsf_value['name'], ratio))
+                distant_matches.append({
+                    "original": subject,
+                    "closest_match": closest_match,
+                    "ratio": ratio
+                })
 
+    print(len(distant_matches))
+    pprint(distant_matches)
     print('exact matches: {}, somewhat same: {}, no close enough match: {}'.format(matches, somewhat_same, similarity_too_low))
     vdb_data.close()
     lsf_data.close()
